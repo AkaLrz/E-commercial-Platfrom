fa5420f1c14b7a811c641e3edb3ff7b08b9c83ca
Runze Li <a397704290@outlook.com>
2023-04-06 17:21:34 -0400

    commodity-app

--- commodity-app/src/main/java/com/skillup/domain/commodity/CommodityRepository.java
+++ commodity-app/src/main/java/com/skillup/domain/commodity/CommodityRepository.java
@@ -1,7 +1,5 @@
 package com.skillup.domain.commodity;
 
-import com.skillup.infrastructure.jooq.tables.records.CommodityRecord;
-
 public interface CommodityRepository {
     void createCommodity(CommodityDomain commodityDomain);
 
